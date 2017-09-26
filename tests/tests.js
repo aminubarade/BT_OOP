36e67bae592c033557c700ca34ce025c3d88c9c8
Bruce Wayne <Ameenub@Ameen.AMEEN>
2017-09-26 16:31:59 +0100

    corrected tests.js

--- tests/tests.js
+++ tests/tests.js
@@ -4,24 +4,24 @@ const myApp = require('../src/main.js');
 describe("aritGeo", function(){
     describe("Handles valid input", function(){
         it("Should return arithmetic", function(){
-        assert.equal(myApp.aritGeo([3, 5, 7, 9, 11, 13, 15]),"arithmetic")
+        assert.equal(Arithmetic.determineSequence([3, 5, 7, 9, 11, 13, 15]),"arithmetic")
         });
         it("Should return Geometric", function(){
-        assert.equal(myApp.aritGeo([2, 4, 8, 16]),"Geometirc")
+        assert.equal(Arithmetic.determineSequence([2, 4, 8, 16]),"Arithmetric")
         });
         it("Should return geomtric", function(){
-            assert.equal(myApp.aritGeo([3,7,11,15]),"Geometric")
+            assert.equal(Arithmetic.determineSequence([2,7,11,15]),"arithmetric")
             });
     })
     describe("Handles invalid input", function(){
         it("Should return 'invalid input'", function(){
-        assert.equal(myApp.aritGeo("demo"),"invalid input")
+        assert.equal(Arithmetic.determineSequence("demo"),"invalid input")
         });
         it("Should fail", function(){
-        assert.equal(myApp.aritGeo([2, 8, 4,7, 133]),-1)
+        assert.equal(Arithmetic.determineSequence([2, 8, 4,7, 133]),-1)
         });
         it("Should return negative 1 (-1)", function(){
-            assert.equal(myApp.aritGeo(" "),0)
+            assert.equal(Arithmetic.determineSequence(" "),0)
             });
     })
 })
